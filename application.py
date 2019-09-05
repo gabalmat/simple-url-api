e86779196ce53d8ec0193294043c0a37f3d5c661
Ginger Balmat <gbalmat.ctr@kr.af.mil>
2019-09-05 00:27:58 -0400

    Update configuration for deployment

--- application.py
+++ application.py
@@ -4,10 +4,11 @@ import os
 
 from app import create_app
 
-config_name = os.getenv('FLASK_CONFIG')
+#config_name = os.getenv('FLASK_CONFIG')
+config_name = 'production'
 
 # create appropriate app object based on FLASK_CONFIG env variable
-app = create_app(config_name)
+application = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    application.run()
--- config.py
+++ config.py
@@ -2,16 +2,15 @@
 
 import os
 
-# db_username = os.getenv('POSTGRES_USER')
-# db_password = os.getenv('POSTGRES_PASSWORD')
-# db_server = os.getenv('DB_SERVER')
-# db_port = os.getenv('DB_PORT')
-# db_name = os.getenv('DB_NAME')
+db_username = os.getenv('POSTGRES_USER')
+db_password = os.getenv('POSTGRES_PASSWORD')
+db_server = os.getenv('DB_SERVER')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
 
 class Config():
     # Common configurations
-    #SQLALCHEMY_DATABASE_URI = f'postgres+psycopg2://{db_username}:{db_password}@{db_server}:{db_port}/{db_name}'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///simple-url'
+    SQLALCHEMY_DATABASE_URI = f'postgres+psycopg2://{db_username}:{db_password}@{db_server}:{db_port}/{db_name}'
 
 class DevelopmentConfig(Config):
     # Development configurations
